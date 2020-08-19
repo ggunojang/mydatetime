3ce9ed9ca32ec28f5e48fa42b57c5dd7aaae9fe5
장건호 <ggunojang@jang-geonhoui-iMac.local>
2020-08-19 18:49:26 +0900

    make right gnb

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React,{useState} from 'react';
-import {Container} from 'react-bootstrap';
-import RealTime from './components/Realtime';
-import Background from './components/Background';
-import Weather from './components/Weather';
-import {MyContext} from './api/MyContext';
-import './App.css';
-
+import React, { useState } from "react";
+import { Container } from "react-bootstrap";
+import RealTime from "./components/Realtime";
+import Background from "./components/Background";
+import Weather from "./components/Weather";
+import Menu from "./components/Menu";
+import { MyContext } from "./api/MyContext";
+import "./App.css";
 
 function App() {
-  const [store, setStore] = useState({backgroundKeyword: ''});
+  const [store, setStore] = useState({ backgroundKeyword: "" });
   return (
-    <MyContext.Provider value={{store, setStore}}>
+    <MyContext.Provider value={{ store, setStore }}>
       <Container fluid>
+        <Menu />
         <Weather />
         <RealTime />
         <Background />
       </Container>
     </MyContext.Provider>
   );
-  
 }
 
 export default App;
