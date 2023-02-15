48816e2b40ef3fbdf0f1aa46877c4dc819a5d028
ggunojang <ggunojang@gmail.com>
2023-02-15 16:04:43 +0900

    백그라운드 호출 변경

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,54 +1,62 @@
-import React, {useState, useEffect, useContext} from 'react';
-import styled from 'styled-components';
-import {setKeyword, MyContext} from '../api/MyContext';
+import React, { useState, useEffect, useContext } from "react";
+import styled from "styled-components";
+import { setKeyword, MyContext } from "../api/MyContext";
+import axios from "axios";
 
 const BGIMG = styled.div`
-    position:fixed;
-    top:0;
-    left:0;
-    right:0;
-    bottom:0;
-    z-index:0;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 0;
     width: 100%;
     height: 100%;
 `;
 
-function ImgRandom(){
-    const {store} = useContext(MyContext);
+const ImgRandom = () => {
+    const { store } = useContext(MyContext);
 
-    const [randomImages, setRandomImages] = useState(`https://source.unsplash.com/1920x1080/?${setKeyword},${store.backgroundKeyword}`);
+    const [randomImages, setRandomImages] = useState(
+        `https://source.unsplash.com/1920x1080/?${setKeyword},${store.backgroundKeyword}`
+    );
 
     useEffect(() => {
-        const randID = setInterval(() => { 
-            fetch(`https://source.unsplash.com/1920x1080/?${setKeyword},${store.backgroundKeyword}`).then((response) => { 
-                setRandomImages(response.url);
-            });
+        const randID = setInterval(() => {
+            axios({
+                method: "GET",
+                url: `https://source.unsplash.com/1920x1080/?${setKeyword},${store.backgroundKeyword}`,
+            })
+                .then((response) => {
+                    const { responseURL } = response.request;
+                    setRandomImages(responseURL);
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
         }, 20000);
         return () => {
-            clearInterval(randID); 
+            clearInterval(randID);
         };
-    },[store]);
+    }, [store]);
 
     return randomImages;
+};
 
-}
-
-function Background (){
+const Background = () => {
     const randomImages = ImgRandom();
     const styles = {
         overflow: `hidden`,
-        backgroundImage:  `url('${randomImages}')`,
+        backgroundImage: `url('${randomImages}')`,
         backgroundRepeat: `no-repeat`,
         backgroundAttachment: `fixed`,
         backgroundPosition: `center center`,
         backgroundSize: `cover`,
         transitionDelay: `0.6s`,
         transition: `0.5s`,
-        transitionTimingFunction: `ease-in-out`
-    }
-    return (
-        <BGIMG style={styles} />
-    )
-}
+        transitionTimingFunction: `ease-in-out`,
+    };
+    return <BGIMG style={styles} />;
+};
 
 export default Background;
